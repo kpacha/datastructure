0444b9ae66ef335b114fb5ac5d52185d78e5720e
Kpacha <kpacha666@gmail.com>
2013-10-26 15:59:00 +0200

    extending the Index interface

--- src/Kpacha/Datastructure/Index.php
+++ src/Kpacha/Datastructure/Index.php
@@ -1,20 +1,34 @@
 <?php
+
 namespace Kpacha\Datastructure;
 
 /**
- * Simple Entity to be indexed
+ * Simple key-value pair to be indexed
  *
  * @author Kpacha <kpacha666@gmail.com>
  */
 class Index
 {
-    
+
     var $key;
     var $value;
-    
+
     public function __toString()
     {
         return "$this->key -> $this->value";
     }
-    
+
+    static public function compare($index1, $index2)
+    {
+        if ($index1->key == $index2->key) {
+            return 0;
+        }
+        return ($index1->key < $index2->key) ? -1 : 1;
+    }
+
+    public function compareWith($index)
+    {
+        return self::compare($this, $index);
+    }
+
 }
