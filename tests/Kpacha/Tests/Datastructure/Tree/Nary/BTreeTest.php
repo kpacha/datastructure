6ec220447fc1086d808ed942ba63db8b5e89ced2
Kpacha <kpacha666@gmail.com>
2013-10-27 09:09:34 +0100

    testing prune

--- tests/Kpacha/Tests/Datastructure/Tree/Nary/BTreeTest.php
+++ tests/Kpacha/Tests/Datastructure/Tree/Nary/BTreeTest.php
@@ -67,6 +67,26 @@ class BTreeTest extends TestCase
         $this->assertEquals(1, $this->_subject->getDepth());
     }
 
+    public function testInsertIntoFirstChildNode()
+    {
+        $indexes = array($this->buildIndex(1), $this->buildIndex(2), $this->buildIndex(3), $this->buildIndex(4));
+        $this->_subject->insert($indexes[1]);
+        $this->_subject->insert($indexes[3]);
+        $this->_subject->insert($indexes[2]);
+        $this->_subject->insert($indexes[0]);
+        $this->assertEquals($indexes, $this->_subject->dump());
+        $this->assertEquals(1, $this->_subject->getDepth());
+    }
+
+    public function testPrune()
+    {
+        $this->testInsertIntoFirstChildNode();
+        $this->_subject->prune();
+        $this->assertTrue($this->_subject->isEmpty());
+        $this->assertEquals(0, $this->_subject->getDepth());
+        $this->assertEquals(array(), $this->_subject->dump());
+    }
+
     private function buildIndex($key, $value = 'some dummy data')
     {
         $entity = new Index;
